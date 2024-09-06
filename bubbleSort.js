7851b70d7aa978f52ea9b7194b196136a8815a5c
AnsaAli <chinjuansa@example.com>
2024-09-06 10:09:56 +0530

    add Bubble sort algorithm

---
+++ bubbleSort.js
@@ -0,0 +1,22 @@
+//Big-O = O(n^2)
+
+function bubbleSort(arr){
+    let swapped;
+    do{
+        
+        swapped = false;
+        for(let i=0; i < arr.length-1; i++){
+
+            if(arr[i] > arr[i+1]){
+                let temp = arr[i];
+                arr[i]= arr[i+1];
+                arr[i+1] = temp;
+                swapped= true;
+            }
+        }
+      
+    }while(swapped);
+}
+const arr=[34,2,5,6,3,23]
+bubbleSort(arr)
+console.log('Bubble sort array',arr)
