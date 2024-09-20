4ec237cdd43ce075993cf15bf526c1ecdd4fa777
AnsaAli <chinjuansa@example.com>
2024-09-20 15:19:46 +0530

    add prepend, and print function

--- linkedListImple.js
+++ linkedListImple.js
@@ -22,4 +22,51 @@ class LinkedList{
     getSize(){
         return this.size;
     }
+
+
+    //add value at the begining of the list
+    prepend(value){
+        //create a node
+        const node = new Node(value);
+
+        //if list is empty
+        if(this.isEmpty()){
+            this.head = node;
+            this.tail = node;
+        }else{
+           node.next = this.head;
+           this.head = node;
+        }
+        //increase the size
+        this.size++;
+    }
+
+    //to print the linked list
+    print(){
+        if(this.isEmpty()){
+            return 'Linked list is empty';
+        }
+
+        let listValues = '';
+        let curr = this.head;
+        while(curr){
+            listValues += `${curr.value } `
+            curr = curr.next;
+        }
+        console.log(listValues)
+
+    }
 }
+
+const linkedList = new LinkedList()
+
+
+linkedList.prepend(10);
+linkedList.print()
+
+linkedList.prepend(20);
+linkedList.prepend(30);
+linkedList.print();
+
+console.log(linkedList.isEmpty())
+console.log(linkedList.getSize())
