63ac598e5b77b0ca5f9b9f23cfd6b65245e4b0a4
AnsaAli <chinjuansa@example.com>
2024-11-11 21:18:34 +0530

    add addEdges func

--- graph.js
+++ graph.js
@@ -4,9 +4,22 @@ class Graph{
     }
 
     addVertex(vertex){
+        //if no vertex, vertex= []
         if(!this.adjList[vertex]){
             this.adjList[vertex] = []
         }
+    }
+
+    addEdges(vertex1,vertex2){
+       //if these vetices in the adjList
+       if(this.adjList[vertex1] && this.adjList[vertex2]){
+        //need to add the vertex1 = vertex2
+        this.adjList[vertex1].push(vertex2);
+        this.adjList[vertex2].push(vertex1);
+
+        return true
+       }
+       return false
     }
 
    
@@ -18,4 +31,7 @@ g.addVertex('A')
 g.addVertex('B')
 g.addVertex('C')
 g.addVertex('D')
+g.addEdges('A','B')
+g.addEdges('A','D')
+g.addEdges('B','C')
 console.log(g)
