16ad8a1932cee679cd7380d8dc0c14f51e0a4551
AnsaAli <chinjuansa@example.com>
2024-12-02 12:04:08 +0530

    add pivot element frist

--- quickSort.js
+++ quickSort.js
@@ -22,3 +22,27 @@ function quickSort(arr) {
 
 const arr = [8, 20, -2, 4, -6];
 console.log(quickSort(arr));
+
+
+function quick(arr){
+
+  let pivot = arr[0];
+  let left = [];
+  let right = [];
+
+  //base case
+  if(arr.length < 2) return arr;
+
+  for(let i=1;i<arr.length;i++){
+     if(arr[i] < pivot){
+      left.push(arr[i])
+     }else{
+      right.push(arr[i])
+     }
+  }
+
+  return [...quick(left), pivot, ...quick(right)]
+}
+const arr1=[34,2,5,6,3,23]
+const an =quick(arr1)
+console.log('quick sort array',an)
